befb0f52a4929ad204ee938cd3eb35f316fbd9f0
Sergio Pinzon <Sergio Pinzon>
2024-12-02 17:16:02 -0500

    Quitando todo de la rama master

--- src/pages/cursos/Cursos.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function Cursos() {
-    return <h1>Gestión de Cursos...</h1>;
-}
--- src/pages/usuarios/Usuarios.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function Usuarios() {
-    return <h1>Gestión de Usuarios...</h1>;
-}
